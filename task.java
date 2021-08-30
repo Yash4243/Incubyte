db18a96e4f3582601250d779c47746f36b703a6a
Yash4243 <63155951+Yash4243@users.noreply.github.com>
2021-08-30 08:37:56 +0530

    Add files via upload

---
+++ task.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class StringCalculator
+{
+	static int Add(String numbers)
+	{
+		if(numbers.equals("")){
+			return 0;
+		}
+		else
+		{
+		    String numList[] = numbers.split(",");
+			
+			int l = numList.length;
+			int sum = 0;
+			
+			for(int i=0; i<l; i++){
+				int val = Integer.parseInt(numList[i]);
+				sum += val;
+			}
+			
+			return sum;
+		}
+	}
+	
+	public static void main(String args[])
+	{
+		int ans;
+		ans = Add("");
+		System.out.println(ans);
+		ans = Add("1");
+		System.out.println(ans);
+		ans = Add("2");
+		System.out.println(ans);
+		ans = Add("1,2");
+		System.out.println(ans);
+	}
+}
