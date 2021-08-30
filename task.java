a2e7422baf4e5f73d07e3c2ecf8475d24e752196
Yash4243 <63155951+Yash4243@users.noreply.github.com>
2021-08-30 09:12:48 +0530

    Add files via upload

--- task.java
+++ task.java
@@ -8,8 +8,14 @@ class StringCalculator
 			return 0;
 		}
 		else
-		{
-		    String numList[] = numbers.split("[,\n]");
+		{	
+			String delimiter = ",";
+			if(numbers.matches("//(.*)\n(.*)")){
+				delimiter = Character.toString(numbers.charAt(2));
+				numbers = numbers.substring(4);
+			}
+
+			String numList[] = numbers.split(delimiter + "|\n");
 			
 			int l = numList.length;
 			int sum = 0;
@@ -36,7 +42,9 @@ class StringCalculator
 		System.out.println(ans);
 		ans = Add("1,2,3,5");
 		System.out.println(ans);
-		ans = Add("1\n2,3\n4");
+		ans = Add("1\n2,3");
+		System.out.println(ans);
+		ans = Add("//;\n1;2");
 		System.out.println(ans);
 	}
 }
